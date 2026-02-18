a80d4dcb057e03a2325ddedbb7d22b8f256d797a
chiaki187 <23fi094@ms.dendai.ac.jp>
2026-02-18 23:32:33 +0900

    draw baseCircle

--- vite-glare_animation1/src/main.ts
+++ vite-glare_animation1/src/main.ts
@@ -1,24 +1,39 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const app=document.getElementById('app') as HTMLElement;
+app.insertAdjacentHTML('afterbegin',`<canvas id="canvas" style="border: solid 1px;" width="480" height="480"></canvas>`);
+const canvas=document.getElementById('canvas') as HTMLCanvasElement;
+const ctx=canvas.getContext('2d') as CanvasRenderingContext2D;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+interface Vector2{
+  x:number;
+  y:number;
+}
+
+interface Glare{
+  draw():void;
+}
+
+abstract class GlareBase implements Glare{
+  protected topLeft:Vector2;
+  constructor(x:number,y:number){
+    this.topLeft={x,y};
+  }
+  getTopLeft():Vector2{return this.topLeft;};
+  abstract draw():void;
+
+}
+
+class Glare1 extends GlareBase{
+  constructor(x:number,y:number){
+    super(x,y);
+  }
+
+  override draw():void{
+    ctx.beginPath();
+    ctx.arc(canvas.width/2,canvas.height/2,3,0,Math.PI*2);
+    ctx.fill();
+  }
+}
+
+const g1:Glare=new Glare1(0,0);
+g1.draw();
